d246300093f316ad1e9ff64cdcf8dc7aeb4b39d4
Kyra Meier <kyraprmeier@gmail.com>
2025-04-16 16:54:40 -0400

    change file name

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.onload = function() {
     "erzuli freda": "assets/erzulifreda.PNG",
     "erzuli danto": "assets/erzulidanto.PNG",
     "oshun": "assets/oshun.PNG",
-    "kyra": "assets/me.jpg"
+    "kyra": "assets/kyra.JPG"
   };
 
   // Load cowry shell image
