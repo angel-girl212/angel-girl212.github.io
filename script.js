a832fbb76727835654e3bbb801fff36d3fca5a6f
Kyra Meier <kyraprmeier@gmail.com>
2025-04-16 17:31:22 -0400

    edit

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ window.onload = function() {
               "kyra": ["fresh fruits", "flowers", "good luck charms", "perfume", "champagne"]
             };
 
-            let offerResponse = prompt(`Congratulations! ${chosenDeity.toUpperCase()} would like to speak with you. What will you offer them?`);
+            let offerResponse = prompt(`Congratulations! ${chosenDeity} would like to speak with you. What will you offer them?`);
             if (!offerResponse) return;
             const offering = offerResponse.toLowerCase();
 
